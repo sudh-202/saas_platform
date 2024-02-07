2f41b67fda72b477954464eebdd4df0156166888
sudhanshu <sri.sudhanshu1@gmail.com>
2024-02-07 18:22:16 +0530

    Conversation Api

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,9 @@
+import { AvatarImage, Avatar } from "@radix-ui/react-avatar"
+
+export const BotAvatar = () => {
+    return (
+        <Avatar className="h-8 w-8">
+            <AvatarImage  className="p-1" src="/logo.png"/>
+        </Avatar>
+    );
+};
