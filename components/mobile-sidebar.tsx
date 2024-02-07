18214bd4e5bb1835b245301751d43d551c186e8f
sudhanshu <sri.sudhanshu1@gmail.com>
2024-02-07 13:16:20 +0530

    conversation AI

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,9 +4,20 @@ import { Button } from "@/components/ui/button";
 import { Menu} from "lucide-react";
 import Sidebar from "@/components/sidebar";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useEffect, useState } from "react";
 
 
 const MobileSidebar = () => {
+    const [isMOunted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMOunted){
+        return null;
+    }
+
     return (
         <Sheet>
             <SheetTrigger>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 import { Montserrat } from "next/font/google";
-import { cn } from "@/lib/utils"
-import { 
-  LayoutDashboard, 
-  MessageSquare, 
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import {
+  LayoutDashboard,
+  MessageSquare,
   ImageIcon,
   VideoIcon,
   Music,
   Settings,
   Code,
- } from "lucide-react";
+} from "lucide-react";
 
 const montserrat = Montserrat({
   weight: "600",
-  subsets: ["latin"]
-}); 
+  subsets: ["latin"],
+});
 
 const routes = [
   {
@@ -64,30 +65,34 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full
-    bg-[#111827] text-white">
+    <div
+      className="space-y-4 py-4 flex flex-col h-full
+    bg-[#111827] text-white"
+    >
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex
-          items-center pl-3 mv-14">
+        <Link
+          href="/dashboard"
+          className="flex
+          items-center pl-3 mb-14"
+        >
           <div className="relative w-8 h-8 mr-4">
-            <Image
-              fill
-              alt="Logo"
-              src="/logo.png"
-            />
+            <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className={cn("text-2xl font-bold", montserrat.className)}>Genius</h1>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Genius
+          </h1>
         </Link>
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
-            href={route.href}
-            key={route.href}
-            className="text-sm group flex p-3 w-full
-            justify-start font-medium cursor-pointer
-            hover:text-white hover:bg-white/10 rounded-lg
-            transition"
+              href={route.href}
+              key={route.href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-5", route.color)} />
@@ -98,7 +103,7 @@ const Sidebar = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
